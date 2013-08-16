8af4b0ca6744eab0d3c41b054ab0b406a8d319da
Frederick Grim <fgrim@celerity.com>
2013-08-16 10:49:55 -0400

    fixed soem tests

--- tests/eop_ch2.py
+++ tests/eop_ch2.py
@@ -18,10 +18,20 @@ class ChapterTwoTest(unittest.TestCase):
 
     def test_collision_point_non_terminating(self):
         from eop.ch2 import collision_point
-        f = lambda x: 10%x
+        f = lambda x: x%10
         self.assertEquals(9, collision_point(9, f))
 
     def test_terminating_non_terminating(self):
         from eop.ch2 import terminating
-        f = lambda x: 10%x
+        f = lambda x: x%10
+        self.assertFalse(terminating(9, f, p=lambda x: x != 10))
+
+    def test_collision_point_terminating(self):
+        from eop.ch2 import collision_point
+        f = lambda x: x%10
+        self.assertEquals(9, collision_point(9, f))
+
+    def test_terminating_non_terminating(self):
+        from eop.ch2 import terminating
+        f = lambda x: x%10
         self.assertFalse(terminating(9, f, p=lambda x: x != 10))
